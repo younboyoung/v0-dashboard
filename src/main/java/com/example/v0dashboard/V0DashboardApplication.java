543ce3c6f97879189f05d73b35a6330d87e902eb
byyoun <byyoun@osci.kr>
2024-02-18 16:09:03 +0900

    first commit

---
+++ src/main/java/com/example/v0dashboard/V0DashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.v0dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V0DashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(V0DashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/v0dashboard/V0DashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.v0dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V0DashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
